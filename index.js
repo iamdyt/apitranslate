bc77c3e6ca74e1e6222670a481ec62d96dc3ace8
iamdyt <olanlokunlanre@gmail.com>
2019-11-03 13:30:09 +0100

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const translate = require('node-google-translate-skidz');
 const express = require ('express');
+const cors = require('cors')
 app = express();
 
 const PORT = process.env.PORT || 7000
 
+app.use(cors())
 app.listen(PORT,()=>{
     console.log('Server started successfully')
 })
