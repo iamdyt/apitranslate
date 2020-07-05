a01d39d8c02f58a6f2e4e9143a253f2634e10f8b
iamdyt <olanlokunlanre@gmail.com>
2020-07-04 17:46:51 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const translate = require('node-google-translate-skidz');
 const express = require ('express');
 const cors = require('cors')
-app = express();
+const path = require('path')
 
+app = express();
+app.use(cors())
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 const PORT = process.env.PORT || 7000
 
-app.use(cors())
-app.listen(PORT,()=>{
-    console.log('Server started successfully')
+
+app.get('/', (req,res)=>{
+  res.render('index')
 })
+
 
 app.get('/translate/:text/:source/:target', (req,res)=>{
 
@@ -24,7 +32,23 @@ app.get('/translate/:text/:source/:target', (req,res)=>{
 
 })
 
+app.get('/translate/:text/:target', (req,res)=>{
 
+  const {text,source, target} = req.params;
+
+  translate({
+      text: text,
+      source: 'auto',
+      target: target
+    }, function(result) {
+      res.send(result);
+    });
+
+})
+
+app.listen(PORT,()=>{
+  console.log('Server started successfully')
+})
 
 
 
